22a1105d3062575e2fab3950e71b74490a6fff98
KimSeungJin <klli1naver@gmail.com>
2023-01-19 22:11:42 +0900

    ⚙️ :: 엔티티 생성

---
+++ schedules.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, VARCHAR, DATE, BINARY, Integer
+from util import Base
+
+class Schedules(Base):
+    __tablename__ = 'schedule'
+
+    id = Column(BINARY(16), primary_key=True)
+    data = Column(DATE, nullable=False)
+    name = Column(VARCHAR(255), nullable=False)
