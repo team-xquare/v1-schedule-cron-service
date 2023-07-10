2558c91690141c43fee7b955c6075758cf923b41
KimSeungJin <klli1naver@gmail.com>
2023-07-10 22:33:13 +0900

    ⚰️ :: 레거시 파이 삭제

--- starter.py
+++
@@ -1,9 +0,0 @@
-from schedule import every, run_pending
-from schedules7day import macro7day
-from schedules30day import macro30days
-
-every(30).days.do(macro30days())
-every().monday.do(macro7day())
-
-while True:
-    run_pending()
