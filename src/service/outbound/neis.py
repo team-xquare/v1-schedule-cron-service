05455974c8b15d666030defdea3b92d3da3c0fd7
KimSeungJin <klli1naver@gmail.com>
2023-07-10 23:24:54 +0900

    ♻️ :: 리뷰 반영

--- src/service/outbound/neis.py
+++ src/service/outbound/neis.py
@@ -4,13 +4,16 @@ import requests
 from config import Config
 from typing import List, Dict
 
+NEIS_URL = "https://open.neis.go.kr/hub/SchoolSchedule?ATPT_OFCDC_SC_CODE=G10&SD_SCHUL_CODE=7430310&pSize=1000&KEY={API_KEY}&Type=json&AA_FROM_YMD={start_date}&AA_TO_YMD={end_date}"
+
 
 def neis_request(start_date: str, end_date: str) -> List[Dict[str, str]]:
     response_data = requests.get(
-        f"https://open.neis.go.kr/hub/SchoolSchedule?ATPT_OFCDC_SC_CODE=G10&SD_SCHUL_CODE=7430310&pSize=1000&KEY={Config.KEY}&Type=json&AA_FROM_YMD={start_date}&AA_TO_YMD={end_date}"
+        NEIS_URL.format(API_KEY=Config.KEY, start_date=start_date, end_date=end_date)
     ).json()
 
     rows = response_data['SchoolSchedule'][1]['row']
-    date_and_events = [{"date": datetime.strptime(row['AA_YMD'], "%Y%m%d").date(), "name": row['EVENT_NM']} for row in rows]
+    date_and_events = [{"date": datetime.strptime(row['AA_YMD'], "%Y%m%d").date(), "name": row['EVENT_NM']} for row in
+                       rows]
 
     return date_and_events
